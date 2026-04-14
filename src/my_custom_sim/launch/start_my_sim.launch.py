030a591c9e8591dca20f641e7c697c7450891501
mega <megachentw0622@gmail.com>
2026-04-15 03:06:03 +0800

    save

--- src/my_custom_sim/launch/start_my_sim.launch.py
+++ src/my_custom_sim/launch/start_my_sim.launch.py
@@ -1,4 +1,5 @@
 import os
+import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
@@ -9,8 +10,17 @@ def generate_launch_description():
     robot_gazebo_dir = get_package_share_directory('robot_gazebo')
     field_dir = get_package_share_directory('Field_description')
 
+    # field_dir 的值會是: .../install/Field_description/share/Field_description
+    # Ignition 需要的是它的上一層目錄: .../install/Field_description/share
+    # =====================================================================
+    field_resource_path = os.path.abspath(os.path.join(field_dir, '..'))
+    
+    if 'IGN_GAZEBO_RESOURCE_PATH' in os.environ:
+        os.environ['IGN_GAZEBO_RESOURCE_PATH'] += ':' + field_resource_path
+    else:
+        os.environ['IGN_GAZEBO_RESOURCE_PATH'] = field_resource_path
+    
     # 1. 啟動官方 Gazebo 環境與機器人
-    # 這裡不覆蓋 'world' 參數，讓官方腳本去讀它自帶的 empty.sdf
     start_gazebo_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(robot_gazebo_dir, 'launch', 'worlds.launch.py')
@@ -20,12 +30,23 @@ def generate_launch_description():
         }.items()
     )
 
-    # 2. 生成我們簡化過 Collision 的場地 (Z 軸設為 0.0)
-    field_urdf_path = os.path.join(field_dir, 'urdf', 'Field.urdf')
+    # 2. 指定你的 場地 Xacro 檔案路徑
+    field_xacro_path = os.path.join(field_dir, 'urdf', 'Field.xacro')
+
+    # 3. 使用 xacro 套件將 .xacro 動態轉換為純 URDF 格式的 XML 字串
+    field_urdf_xml = xacro.process_file(field_xacro_path).toxml()
+
+    # 4. 使用「新版 Gazebo (ros_gz_sim)」的生成節點，而非舊版的 gazebo_ros
     spawn_field_node = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-entity', 'my_field', '-file', field_urdf_path, '-x', '0.0', '-y', '0.0', '-z', '0.0'],
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'my_field',
+            '-string', field_urdf_xml,  # 直接傳入轉換好的 XML 字串
+            '-x', '0.0', 
+            '-y', '0.0', 
+            '-z', '1.0'
+        ],
         output='screen'
     )
 
